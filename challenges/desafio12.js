dd3129c915ea0cb262a2901a718691c624df018c
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-09-26 18:23:31 -0300

    n entendi pq n foi

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,6 +1,6 @@
 use("aggregations");
 db.trips.aggregate([
-  { $group: { _id: { day: { $dayOfWeek: "$startTime" }, estacaoNome: "$startStationName" }, soma: { $sum: 1 } } },
+  { $group: { _id: { dia: { $dayOfWeek: "$startTime" }, estacaoNome: "$startStationName" }, soma: { $sum: 1 } } },
   { $project: { nomeEstacao: "$_id.estacaoNome", total: "$count", _id: 0 } },
   { $sort: { total: -1 } },
   { $limit: 1 },
