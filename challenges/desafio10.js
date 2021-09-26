efd2ff3a4d3de947d4713101ebc9e0d2373ce381
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-09-26 16:33:37 -0300

    completando 11 e 12

---
+++ challenges/desafio10.js
@@ -0,0 +1,13 @@
+db.trips.aggregate([
+  { $group: {  
+    _id: "$usertype",
+    horas: { $avg: { $subtract: ["$stoptime", "$starttime"] } },
+  } },
+  { 
+    $project: {
+    _id: 0
+    tipo: "$_id",
+    duracaoMedia: { $round: [{ $divide: ["$horas", 3600000] }, 2] },
+  } },
+  { $sort: { duracaoMedia: 1 } },
+]);
