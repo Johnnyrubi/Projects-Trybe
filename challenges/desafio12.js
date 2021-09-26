3d14b44def3078e76f33dfbedcb6eb0f5dff3f3c
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-09-26 18:48:27 -0300

    resolvido 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,7 +1,7 @@
 use("aggregations");
 db.trips.aggregate([
-  { $group: { _id: { dia: { $dayOfWeek: "$startTime" }, estacaoNome: "$startStationName" }, soma: { $sum: 1 } } },
-  { $project: { nomeEstacao: "$_id.estacaoNome", total: "$count", _id: 0 } },
+  { $group: { _id: { day: { $dayOfWeek: "$startTime" }, estacaoNome: "$startStationName" }, soma: { $sum: 1 } } },
+  { $project: { nomeEstacao: "$_id.estacaoNome", total: "$soma", _id: 0 } },
   { $sort: { total: -1 } },
   { $limit: 1 },
 ]);
