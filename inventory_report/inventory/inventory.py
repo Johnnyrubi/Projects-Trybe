554ebfb91123f18d0fb2a365d0e5835fde341a5a
Johnny Rubi <johnnyrubischreiner@gmail.com>
2022-02-03 14:38:22 -0300

    requisito 4 passando

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,13 +1,17 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory():
     @staticmethod
     def import_data(path, tipo):
         with open(path, mode="r") as file:
-            status_reader = list(csv.DictReader(file))
+            if path.endswith(".csv"):
+                status_reader = list(csv.DictReader(file))
+            if path.endswith(".json"):
+                status_reader = json.load(file)
             if tipo == "simples":
                 return SimpleReport.generate(status_reader)
             else:
