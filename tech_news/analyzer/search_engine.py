9a496820c1f01328318e33b436cc843dbf0b7721
Johnny Rubi <johnnyrubischreiner@gmail.com>
2022-02-17 20:01:30 -0300

    requisito 7 feito

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 import re
+import datetime
 from tech_news.database import search_news
 
 # Requisito 6
@@ -7,9 +8,9 @@ from tech_news.database import search_news
 def search_by_title(title):
     """Seu código deve vir aqui"""
     new_itens = []
-    all_news = search_news({"title": re.compile(title, re.IGNORECASE)})
+    news = search_news({"title": re.compile(title, re.IGNORECASE)})
 
-    for item in all_news:
+    for item in news:
         new_itens.append((item["title"], item["url"]))
     return new_itens
 
@@ -17,6 +18,12 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        datetime.date.fromisoformat(date)
+        news = search_news({"timestamp": re.compile(date)})
+        return [(item["title"], item["url"]) for item in news]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
