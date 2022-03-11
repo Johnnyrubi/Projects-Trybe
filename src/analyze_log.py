45e719561e5c19f3937c9883e2e4e34d818baefb
Johnny Rubi <johnnyrubischreiner@gmail.com>
2022-03-11 16:53:47 -0300

    arquivo csv sendo lido

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,16 @@
+import csv
+
+
+def reading(path_to_file):
+    lista_vazia = []
+    with open(path_to_file) as file:
+        keys = ["nome", "comida", "dia"]
+        status_reader = csv.DictReader(file, delimiter=",", fieldnames=keys)
+        for item in status_reader:
+            lista_vazia.append(item)
+    return lista_vazia
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    fileRead = reading(path_to_file)
+    print(fileRead)
