aeab9764a7354c13ae63b7c4b77c74ca45538ac4
Johnny Rubi <johnnyrubischreiner@gmail.com>
2022-03-12 00:22:29 -0300

    primeiro requisito feito

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -11,6 +11,39 @@ def reading(path_to_file):
     return lista_vazia
 
 
+def countMariaFood(arrayList):
+    contador = [i["comida"] for i in arrayList if i["nome"] == "maria"]
+    return max(contador, key=contador.count)
+
+
+def countArnaldoBurguer(arrayList):
+    contador = [i["comida"] for i in arrayList if i["nome"] == "arnaldo"]
+    return contador.count("hamburguer")
+
+
+def countJoaoFoodNeverDid(arrayList):
+    count = {i["comida"] for i in arrayList}
+    contador = [i["comida"] for i in arrayList if i["nome"] == "joao"]
+    return count.difference(contador)
+
+
+def daysJoaoNotGo(arrayList):
+    count = {i["dia"] for i in arrayList}
+    contador = [i["dia"] for i in arrayList if i["nome"] == "joao"]
+    return count.difference(contador)
+
+
 def analyze_log(path_to_file):
     fileRead = reading(path_to_file)
-    print(fileRead)
+    mariaResult = countMariaFood(fileRead)
+    arnaldoResult = countArnaldoBurguer(fileRead)
+    joaoResult = countJoaoFoodNeverDid(fileRead)
+    joaoDays = daysJoaoNotGo(fileRead)
+
+    with open("data/mkt_campaign.txt", mode="w") as f:
+        f.writelines([
+            f"{mariaResult}\n"
+            f"{arnaldoResult}\n"
+            f"{joaoResult}\n"
+            f"{joaoDays}\n"
+        ])
